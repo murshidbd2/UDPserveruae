073836f72e30d224244ed8b41d6fa777121214f9
Murshid Ali <149285103+murshidbd2@users.noreply.github.com>
2023-11-04 23:36:10 +0600

    Update UDPserver.sh

--- UDPserver.sh
+++ UDPserver.sh
@@ -5,7 +5,7 @@ lang_dir="$udp_file/lang"
 lang="$lang_dir/lang"
 
 idioam_lang(){
-  source <(curl -sSl 'https://raw.githubusercontent.com/murshidbd2/UDPserveruae/main/lang')
+  source <(curl -sSl 'https://raw.githubusercontent.com/rudi9999/SocksIP-udpServer/main/lang/lang')
   title -ama 'IDIOMA/LANGUAGE'
   echo " $(msg -verd "[0]") $(msg -verm2 '>') $(msg -azu "English Default")"
   n=0
